b9b096e8e10db198d6b32d165a21a7c8010a184d
aguptaindia2012 <aguptaindia2012@gmail.com>
2026-06-23 21:52:30 +0530

    Add configuration for Supabase connection

---
+++ config.js
@@ -0,0 +1,4 @@
+window.DCB_CONFIG = {
+       SUPABASE_URL: https://kxggfqkofhfyalaeyyyl.supabase.co,
+       SUPABASE_ANON_KEY: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4Z2dmcWtvZmhmeWFsYWV5eXlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDQ4MTcsImV4cCI6MjA5Nzc4MDgxN30.vn6g5ONpyJMbT4v9YXiZoDFbiusq80KWec7UW4TXvpY
+     };
